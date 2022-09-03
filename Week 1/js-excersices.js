80b6155fc1d48e6fdcb6240844a49381a14396a7
Rafael Peraza <rdavid_peraza@live.com>
2022-09-03 13:15:29 -0700

    Create js-excersices.js

---
+++ Week 1/js-excersices.js
@@ -0,0 +1,4 @@
+![Core-code](https://user-images.githubusercontent.com/109502959/180068652-17c363d8-9f97-4dca-a527-fd096a040a8e.png)
+
+# Core-code-from-scratch-readme
+This readme is focus on **Software development fundamentals.**
